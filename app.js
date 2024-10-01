a05a7f8201246443af28274363a8930e6b3734b4
uzair <uzairparkar1@gmail.com>
2024-10-01 13:26:27 +0530

    Detective selection and return.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const frames = [
-    `C`, `Click`, `Click Me`, ``,  `Click Me To`, `Click Me To Play`, ``, `Click Me To Play`, ``,  `Click Me To Play`,  ``, `Click Me To Play`, ``,
+`C`,`Click`,`Click Me`,``,`Click Me To`,`Click Me To Play`,``,`Click Me To Play`,``,`Click Me To Play`,``,`Click Me To Play`,``,
 ]
 let currentFrame = 0;
     const asciiElement = document.getElementById('title')
@@ -19,6 +19,38 @@ function begin(){
     document.getElementById('Wrapper').style.borderColor = 'yellowgreen'
     document.getElementById('title').style.display = "none"
     document.getElementById('title').style.border = "none"
+    document.getElementById('TitleOne').style.display = "block"
 }
+ function sceneshow(){
+    section = document.getElementsByClassName("sceneone")  
+for (let i=0;i<section.length;i++){
+    section[i].style.display = "block"
+}
+section2 = document.getElementById('sceneonetitle')
+section2.style.display = "block"
 
+section3 = document.getElementById("empty1")
+section3.style.display = "block"
+section4= document.getElementById("empty2")
+section4.style.display = "block"
+section5 = document.getElementById("empty3")
+section5.style.display = "block"
+ }
+  function showstrydetective(){
+    detective_1 = document.getElementById("detective")
+    detective_1.style.display = "block"
+    section  = document.getElementsByClassName("sceneone")
+    for(let i = 0;i<section.length;i++){
+        section[i].style.display = "none"
+    }
+    section2 = document.getElementById('sceneonetitle')
+    section2.innerHTML = 'Detective Backstory'
+  }
+  function back(){
+    sceneshow()
+     section2 = document.getElementById('sceneonetitle')
+    section2.innerHTML = 'Please Select Your Character'
+     detective_1 = document.getElementById("detective")
+    detective_1.style.display = "none"
 
+  }
