f066241b2fac4ff74458dd48250718db1de41124
uzair <uzairparkar1@gmail.com>
2024-10-01 22:39:26 +0530

    The story is about to start.

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const frames = [
 `C`,`Click`,`Click Me`,``,`Click Me To`,`Click Me To Play`,``,`Click Me To Play`,``,`Click Me To Play`,``,`Click Me To Play`,``,
 ]
 let currentFrame = 0;
+// sets current frame to zero
     const asciiElement = document.getElementById('title')
+    // gets the container for the art
     asciiElement.classList.add = `title`;
-
+    // adds a class to container for styling 
     function animateASCIIArt() {
+        // fucntion  to animate the art
         asciiElement.textContent = frames[currentFrame];
+        // sets the text content to frames
         currentFrame = (currentFrame + 1) % frames.length;
+    // the current frame adds the frames everytime the function runs.
     }
 
     setInterval(animateASCIIArt, 500);
@@ -46,7 +51,7 @@ section5.style.display = "block"
     section2 = document.getElementById('sceneonetitle')
     section2.innerHTML = 'Detective Backstory'
   }
-  function back(){
+  function back1(){
     sceneshow()
      section2 = document.getElementById('sceneonetitle')
     section2.innerHTML = 'Please Select Your Character'
@@ -54,3 +59,42 @@ section5.style.display = "block"
     detective_1.style.display = "none"
 
   }
+   function showstryculprit(){
+    culprit_1 = document.getElementById('culprit')
+    culprit_1.style.display = 'block'
+    section  = document.getElementsByClassName("sceneone")
+    for(let i = 0;i<section.length;i++){
+        section[i].style.display = "none"
+    }
+
+    section2 = document.getElementById('sceneonetitle')
+    section2.innerHTML = 'Culprit Backstory'
+   }
+   function back2(){
+    sceneshow()
+     section2 = document.getElementById('sceneonetitle')
+    section2.innerHTML = 'Please Select Your Character'
+     detective_1 = document.getElementById("culprit")
+    detective_1.style.display = "none"
+
+  }
+   function showstryhero(){
+   hero_1 = document.getElementById('hero')
+   hero_1.style.display = 'block'
+   section  = document.getElementsByClassName("sceneone")
+   for(let i = 0;i<section.length;i++){
+       section[i].style.display = "none"
+   }
+
+   section2 = document.getElementById('sceneonetitle')
+   section2.innerHTML = 'Hero Backstory'
+  }
+   function back3(){
+    sceneshow()
+     section3 = document.getElementById('sceneonetitle')
+    section3.innerHTML = 'Please Select Your Character'
+     hero_1 = document.getElementById("hero")
+    hero_1.style.display = "none"
+  }
+
+  //Story begins here.
